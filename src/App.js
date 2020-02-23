86702abeacd70d35889e04f5726ae153edd6550a
NaJthaGod <35042135+NaJthaGod@users.noreply.github.com>
2020-02-22 20:26:22 -0500

    re-do

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import Todos from './components/Todos';
+import Header from './components/layout/Header';
+import AddTodo from './components/AddTodo';
+import uuid from 'uuid';
 import './App.css';
+
+
 
 class App extends Component {
   state = {
     todos: [
       {
-        id: 1,
+        id: uuid.v4(),
         title: 'Take out the trash',
         completed: false
       },
@@ -24,12 +29,41 @@ class App extends Component {
       }
     ]
   }
+  //toggle completed
+  markComplete =(id)=>{
+    this.setState({ todos: this.state.todos.map(todo => {
+      if(todo.id === id){
+        todo.completed = !todo.completed
+      }
+      return todo;
+    }) });
+  }
+
+  //Delete Todo
+  delTodo = (id)=> {
+    this.setState({todos: [...this.state.todos.filter(todo => todo.id !== id)]})
+  }
+
+  //Add Todo
+  addTodo =(title) => {
+    const newTodo= {
+      id: uuid.v4(),
+      title: title,
+      completed: false
+    }
+    this.setState({todos: [...this.state.todos, newTodo]})
+  }
+
   render(){
     
     return (
       <div className="App">
-      
-      <Todos todos={this.state.todos} />
+        <div className="container">
+      <Header />
+      <AddTodo addTodo={this.addTodo}/>
+      <Todos todos={this.state.todos} markComplete={this.markComplete} 
+      delTodo={this.delTodo}/>
+      </div>
       </div>
     )
   }
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -2,9 +2,11 @@ import React, { Component } from 'react';
 import TodoItem from './TodoItem';
 import PropTypes from 'prop-types';
 class Todos extends Component {
+   
+
     render(){
       return this.props.todos.map((todo)=>{
-      return <TodoItem key={todo.id} todo={todo} />
+      return <TodoItem key={todo.id} todo={todo} markComplete={this.props.markComplete} delTodo={this.props.delTodo} />
       })
     }
   }
